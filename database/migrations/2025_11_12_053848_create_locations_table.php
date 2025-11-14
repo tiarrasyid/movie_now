d3b3a9f6ad87c2b5626f76b3f97d3b87b58f8df5
tiarrasyid <tiarrasyid2298@gmail.com>
2025-11-14 23:40:21 +0700

    sedikit perbaikan

--- database/migrations/2025_11_12_053848_create_locations_table.php
+++ database/migrations/2025_11_12_053848_create_locations_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('loactions', function (Blueprint $table) {
+        Schema::create('locations', function (Blueprint $table) {
             $table->id('location_id');
             $table->unsignedBigInteger('movie_id'); // FK ke movies
             $table->string('location_name', 50);
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('loactions');
+        Schema::dropIfExists('locations');
     }
 };
